022290745158a621236c082697c7468890486b6f
aicha-azr <aichaazeroual03@gmail.com>
2024-07-30 17:40:44 +0100

    Popularjobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react'
 import styles from './popularjobs.style'
-
+import { View, Text, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import { SIZES } from '../../../constants'
 const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+          <Text  style={styles.headerTitle}>Popular jobs</Text>
+          <TouchableOpacity >
+            <Text style={styles.headerBtn}>Show all</Text>
+          </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading? (
+          <ActivityIndicator  size='large' colors={COLORS.primary}/>
+        ): error ? (
+          <Text>Something went wrong</Text>
+        ): (
+          <FlatList 
+          data={[1, 2, 3, 4]}
+          renderItem={({item}) => (
+            <PopularJobCard item={item}
+           />
+          )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{ columnGap: SIZES.medium}}
+          horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
